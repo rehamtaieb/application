80e93e57e63230fdf86b283ddf4ab0542b67d94a
Rihem Matoussi <61407411+rehamtaieb@users.noreply.github.com>
2024-05-25 19:55:01 +0100

    Update Reglement.service.ts

--- Front/crud-tuto-front/src/app/shared/Service/Reglement.service.ts
+++ Front/crud-tuto-front/src/app/shared/Service/Reglement.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ReglementService {
-  readonly API_URL = 'http://192.168.49.100:8089/SpringMVC/reglement';
+  readonly API_URL = 'http://192.168.49.101:8089/SpringMVC/reglement';
 
   constructor(private httpClient: HttpClient) {
   }
