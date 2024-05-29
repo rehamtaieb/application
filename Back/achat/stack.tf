4602424ce939319d1ee8c0512768b4842e985f07
ridha_boulares <mohamedridha.boulares@esprit.tn>
2024-05-29 14:32:07 +0100

    ansible and terraform private cloud

--- Back/achat/stack.tf
+++ Back/achat/stack.tf
@@ -16,15 +16,25 @@ provider "openstack" {
   region      = "RegionOne"
 }
 
+# Generate an SSH key pair
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "openstack_compute_keypair_v2" "ssh_key" {
+  name       = "ssh-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
 
 resource "openstack_compute_instance_v2" "Cirros" {
   name            = "Ubuntu Minikube"
-  image_id        = "9029164d-ab4a-4914-9da9-461512fee49d"
+  image_id        = "acfe17f3-8d3a-484f-9975-f670cf750ffa"
   flavor_id       = "d3"
-  key_pair        = "ssh-key"
+  key_pair        = openstack_compute_keypair_v2.ssh_key.name
   security_groups = ["default"]
   network {
-    name = "private"
+    name = "shared"
   }
 }
 
@@ -35,4 +45,10 @@ resource "openstack_networking_floatingip_v2" "fip" {
 resource "openstack_compute_floatingip_associate_v2" "fip_assoc" {
   floating_ip = openstack_networking_floatingip_v2.fip.address
   instance_id = openstack_compute_instance_v2.Cirros.id
+}
+
+# Output the private key
+output "private_key" {
+  value     = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
 }
