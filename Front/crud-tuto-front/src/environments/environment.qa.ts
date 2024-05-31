fb6ddc9f1623a30e55c6ca821ca00d83097f38b2
Rihem Matoussi <61407411+rehamtaieb@users.noreply.github.com>
2024-05-31 21:23:16 +0100

    Update environment.qa.ts

--- Front/crud-tuto-front/src/environments/environment.qa.ts
+++ Front/crud-tuto-front/src/environments/environment.qa.ts
@@ -1,6 +1,5 @@
-  ```typescript
-   export const environment = {
+ export const environment = {
      production: false,
      apiUrl: 'http:// 192.168.49.103:8089/SpringMVC/facture' // Define QA API URL here
    };
-   ```
+   
