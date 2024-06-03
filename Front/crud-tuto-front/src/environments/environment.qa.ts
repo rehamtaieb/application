b89110800a7c876a9dee4756aba4514dbf2e9d24
Rihem Matoussi <61407411+rehamtaieb@users.noreply.github.com>
2024-06-03 18:16:47 +0100

    Update environment.qa.ts

--- Front/crud-tuto-front/src/environments/environment.qa.ts
+++ Front/crud-tuto-front/src/environments/environment.qa.ts
@@ -1,8 +1,6 @@
  export const environment = {
      production: false,
      apiUrl: 'http://192.168.49.103:8089',
-     environmentName: 'qa',
-     console.log('Environment:', environment.environmentName);
-
-   };
+     environmentName: 'qa'
+ };
    
